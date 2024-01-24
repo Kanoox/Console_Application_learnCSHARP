e80ee6c9987be47116a64d75886ed6d4825cbc3c
Kanoox <tintinbinet@hotmail.fr>
2024-01-24 09:51:56 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quel est votre prénom :");
+            string prenom = Console.ReadLine();
+
+            Console.Write("Quel est votre âge :");
+            string age = Console.ReadLine();
+
+            Console.WriteLine($"Bonjour, {prenom} vous avez actuellement {age} ans");
+        }
+    }
+}
